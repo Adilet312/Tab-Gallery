526dd16dcaa71df31f66f94ebd5c1714c68b3698
Adilet312 <adiletm@hotmail.com>
2020-08-25 00:01:00 -0700

    Add tab gallery.

---
+++ src/tab_gallery.js
@@ -0,0 +1,88 @@
+import './css/styles.css';
+import img_lights from "./assets/img_lights.jpg";
+import img_mountains from "./assets/img_mountains.jpg";
+import img_nature from "./assets/img_nature.jpg";
+import img_snow from "./assets/img_snow.jpg";
+const images = function(){
+  let pictures = document.querySelector('.pictures').children;
+  for(let index = 0; index < pictures.length; index++){
+    let img = document.createElement('img');
+    if(index === 0){
+      img.src = `${img_lights}`;
+      img.id=`${index}`
+    }
+    else if(index === 1){
+      img.src = `${img_mountains}`;
+      img.id=`${index}`
+    }
+    else if(index === 2){
+      img.src = `${img_nature}`;
+      img.id=`${index}`
+    }
+    else if(index ===3){
+      img.src = `${img_snow}`;
+      img.id=`${index}`
+    }
+    else{
+      img.src = "";
+      img.id=`${index}`
+    }
+    pictures[index].appendChild(img);
+  }
+};
+images();
+
+function getLargePicture(id) {
+  // Get the expanded image
+  var img = document.querySelector('.bgPicture img');
+  console.log(img)
+  // Use the same src in the expanded image as the image being clicked on from the grid
+  if(Number(id) === 0){
+    img.setAttribute('src',`${img_lights}`);
+
+  }
+  else if(Number(id) === 1){
+    img.src = `${img_mountains}`;
+  }
+  else if(Number(id) === 2){
+    img.src = `${img_nature}`;
+  }
+  else if(Number(id)===3){
+    img.src = `${img_snow}`;
+  }
+  console.log(img)
+  // Show the container element (hidden with CSS)
+  img.parentElement.style.display = "block";
+}
+document.querySelector('.pictures').addEventListener("click", function(e){
+  console.log(e.target.id)
+  getLargePicture(e.target.id)
+},false);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+let overlay =  document.createElement('div');
+overlay.className = "overlayClass";
+overlay.style.position = "absolute";
+overlay.style.top = window.pageYOffset + "px";
+overlay.style.left =  window.pageXOffset + "px";
+overlay.style.background = 'rgba(0,0,0,0.7)';
+overlay.style.width = window.innerWidth + "px";
+overlay.style.height = window.innerHeight + "px";
+*/
---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+
+
+module.exports = {
+  entry: './src/tab_gallery.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  devtool: 'eval-source-map',
+  devServer: {
+    contentBase: './dist'
+  },
+  plugins: [
+    new UglifyJsPlugin({ sourceMap: true }),
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'Tab Gallery',
+      template: './src/index.html',
+      inject: 'body'
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.(jpg|jpeg?g|gif)$/,
+        use: {
+          loader: 'url-loader',
+        },
+     },
+      {
+        test: /\.js$/,
+        exclude: [
+            /node_modules/,
+            /spec/
+          ],
+        loader: "eslint-loader"
+      }
+    ]
+  }
+};
